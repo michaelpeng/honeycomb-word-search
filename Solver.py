1798ece72eeee684a981b79e54a9181b39c22111
michaelpeng@berkeley.edu <michaelpeng@berkeley.edu>
2016-01-09 14:07:11 -0800

    Removed docstring after moving into readme

--- Solver.py
+++ Solver.py
@@ -2,48 +2,6 @@ import sys
 import getopt
 import fileinput
 
-"""
-Quantcast Programming Question: Honeycomb Word Search
-
-Input:
-honeycomb.txt: Line 1: # of layers; Line 2+: Each layer (clockwise) rotating through graph
-dictionary.txt: Each line contains a valid word for which the program is to search
-
-Output:
-Alphabetically sorted list of valid strings found in honeycomb (printed to stdout)
-
-Run:
-> python Solver.py honeycomb.txt dictionary.txt
-
-Misc notes:
-Cells cannot be used twice in the formation of a single string.
-If a valid string can be found via multiple paths, print that string only once.
-
-Approach summary:
-1) Construct graph from honeycomb file:
-    a) Separate graph into 6 triangles*, overlapping in line of nodes
-        drawn from center node out to each corner of hexagon
-    b) Each triangle's layer of edges proceeding outward = indexed range of each honeycomb layer
-    c) For each pair of adjacent nodes in subarray layer, plus adjacent node in inner layer:
-        set corresponding neighborship among nodes (these 3 nodes form a triangle)**
-    d) Rotate triangle-wise through graph, changing only:
-        i)  subarray indexing
-        ii) which neighbor directions are set in each grouping
-
-2) Search for each word in dictionary
-    a) Use dictionary mapping first-letters of valid words to their respective words
-    b) Also use dictionary mapping first-letters of valid words to nodes with that letter
-    c) Use BFS: search for each word in graph
-        i)   get hashed list of words for each given first-letter key
-        ii)  get hashed list of nodes for each given first-letter key
-        iii) perform BFS for each word in i) from each node in ii)
-
-*   Boundaries of triangle 0: center node A extending up to L, A extending northeast to G
-    Boundaries of triangle 1: A extending northeast to G, A extending southeast to X, etc.
-**  In triangle 1: first grouping is B,C,A (A is innermost node);
-    in next layer of triangle 1: U,A,B and A,N,C, etc.
-"""
-
 # Parse honeycomb.txt file for the layers
 honeycomb = [list(item) for item in open(sys.argv[1]).read().splitlines()]
 
